566b47881eb0450740bfa22327837e57641068d0
unknown <christopher.roelofs@gmail.com>
2022-01-30 08:57:46 -0500

    add bat scripts

--- build_extra.py
+++ build_extra.py
@@ -10,4 +10,6 @@ except Exception as e:
     pass
 os.system('pyinstaller -i icon.ico -n DatabaseManager --clean -y -F --windowed database_manager.py')
 os.system('pyinstaller -i icon.ico -n DatabaseTool --clean -y -F  database_tool.py')
+shutil.copyfile('Create Patch.bat', '{0}/Create Patch.bat'.format("dist"))
+shutil.copyfile('Import Patch.bat', '{0}/Import Patch.bat'.format("dist"))
 shutil.make_archive(os.path.join("release","extra"), 'zip', "dist")
